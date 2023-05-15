2322ccdcf51f88e7d740835948c0bc9889003824
Maradja Aryelle <125324662+MaradjaAryelle@users.noreply.github.com>
2023-05-15 16:03:12 -0300

    Add files via upload

---
+++ DivisaoPorZero/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int num1 = 10;
+            int num2 = 0;
+            int resultado = num1 / num2;
+            System.out.println("O resultado é: " + resultado);
+        } catch (ArithmeticException e) {
+            System.err.println(e);
+        }
+    }
+}
